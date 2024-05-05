068ca045ca4030fc2cb226701afaaa5c7fda703b
Santu Ghosh <santutsk@gmail.com>
2024-05-05 19:38:11 +0530

    completed send recv test

--- KafkaDotNetTest/Program.cs
+++ KafkaDotNetTest/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Confluent.Kafka;
+using Kafka.Public;
+using Kafka.Public.Loggers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Text;
 
 namespace KafkaDotNetTest
 {
@@ -16,15 +19,62 @@ namespace KafkaDotNetTest
             Host.CreateDefaultBuilder(args)
             .ConfigureServices((context, collection) =>
             {
+                collection.AddHostedService<KafkaConsumerHostedService>();
                 collection.AddHostedService<KafkaProducerHostedService>();
             });
     }
 
+    /// <summary>
+    /// Kafka Cosumer Hosted Service
+    /// </summary>
+    public class KafkaConsumerHostedService : IHostedService
+    {
+        private readonly ILogger<KafkaConsumerHostedService> _logger;
+        private ClusterClient _cluster;
+
+        public KafkaConsumerHostedService(ILogger<KafkaConsumerHostedService> logger)
+        {
+            this._logger = logger;
+            this._cluster = new ClusterClient(new Configuration
+            {
+                Seeds = "localhost:9092"
+            }, new ConsoleLogger());
+        }
+
+        /// <summary>
+        /// start Async
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            this._cluster.ConsumeFromLatest("test");
+            this._cluster.MessageReceived += record =>
+            {
+                this._logger.LogInformation($"Received {Encoding.UTF8.GetString(record.Value as byte[])}");
+            };
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            this._cluster?.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+
+    /// <summary>
+    /// Kafka Producer Hosted Service
+    /// </summary>
     public class KafkaProducerHostedService : IHostedService
     {
         public readonly ILogger<KafkaProducerHostedService> _logger;
         private IProducer<Null, string> _producer;
 
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="logger"></param>
         public KafkaProducerHostedService(ILogger<KafkaProducerHostedService> logger)
         {
             this._logger = logger;
@@ -46,6 +96,8 @@ namespace KafkaDotNetTest
                 {
                     Value = value
                 },cancellationToken) ;
+                
+                Thread.Sleep(100);
             }
 
            this._producer.Flush(TimeSpan.FromSeconds(5));
